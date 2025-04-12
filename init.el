75986ec8bd4932972a71b95fbccfb483b62c6d2f
categoricalcat <catufuzgu@gmail.com>
2025-04-12 09:27:20 -0300

    Add initial Emacs configuration and setup EditorConfig and .gitignore

---
+++ init.el
@@ -0,0 +1,12 @@
+;;; init.el --- Emacs configuration
+
+;;; Commentary:
+;;; Code:
+
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(column-number-mode 1)
+
+; (setq inhibit-startup-message t)
+
