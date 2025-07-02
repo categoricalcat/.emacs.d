5e2ee99c6752922dae06be2fafa9abfc245a7179
categoricalcat <catufuzgu@gmail.com>
2025-07-02 16:09:10 -0300

    chore: fixing install script for already installed

--- services/install-linux.sh
+++ services/install-linux.sh
@@ -2,6 +2,14 @@
 set -e
 
 mkdir -p ~/.config/systemd/user
+
+if [[ -f ~/.config/systemd/user/emacs-daemon.service ]]; then
+  systemctl --user stop emacs-daemon.service
+  systemctl --user disable emacs-daemon.service
+  rm -f ~/.config/systemd/user/emacs-daemon.service
+  systemctl --user daemon-reload
+fi
+
 cp emacs-daemon.service ~/.config/systemd/user/
 systemctl --user daemon-reload
 systemctl --user enable emacs-daemon.service
--- services/install-macos.sh
+++ services/install-macos.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+# Create LaunchAgents directory if it doesn't exist
 mkdir -p ~/Library/LaunchAgents
+
+# if cp already exists, remove it
+if [[ -f ~/Library/LaunchAgents/org.gnu.emacs.daemon.plist ]]; then
+  echo "Unloading Emacs daemon"
+  launchctl unload ~/Library/LaunchAgents/org.gnu.emacs.daemon.plist
+  rm -f ~/Library/LaunchAgents/org.gnu.emacs.daemon.plist
+fi
+
+echo "Copying Emacs daemon plist"
 cp org.gnu.emacs.daemon.plist ~/Library/LaunchAgents/
+
+echo "Loading Emacs daemon"
 launchctl load ~/Library/LaunchAgents/org.gnu.emacs.daemon.plist
