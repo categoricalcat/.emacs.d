14a1c67aeaf1dbc55a3e5c075b26d8a05851b5b0
categoricalcat <catufuzgu@gmail.com>
2025-07-05 14:58:58 -0300

    feat: add force_kill_emacs function to service-emacs.sh for port management

--- scripts/service-emacs.sh
+++ scripts/service-emacs.sh
@@ -2,6 +2,11 @@
 
 service_dir=~/.config/systemd/user
 
+function force_kill_emacs() {
+  # lsof kill all 13245 port
+  lsof -i :13245 | awk 'NR>1 {print $2}' | xargs kill -9
+}
+
 function start_daemon() {
   echo "Starting emacs service"
 
