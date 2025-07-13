503e4c4f3e39a70ae78a72e272f3fe6a360ba225
categoricalcat <catufuzgu@gmail.com>
2025-07-13 19:27:32 +0000

    Update tramp

--- lisp/femacs-tramp.el
+++ lisp/femacs-tramp.el
@@ -8,14 +8,21 @@
 (require 'tramp)
 
 ;; Set default SSH method (recommended)
-(setq tramp-default-method "ssh")
+(setq tramp-default-method "sshx")
 
 ;; Optional: Reuse SSH connections for speed (uses ControlMaster)
-(setq tramp-use-ssh-controlmaster-options t)
+(setq tramp-use-ssh-controlmaster-options nil)
 
-(setq tramp-default-user "fufu")
+(setq tramp-default-user "fufu"
+  tramp-verbose 6
+  password-cache-expiry 3600
+  tramp-ssh-controlmaster-options "")  ; Ensure ControlMaster is disabled
 
-(setq password-cache-expiry 3600)
+;; Verify TRAMP settings on load
+(add-hook 'after-init-hook
+  (lambda ()
+    (message "TRAMP method: %s (sshx recommended for Prelude)"
+      tramp-default-method)))
 
 (provide 'femacs-tramp)
 ;;; femacs-tramp.el ends here
